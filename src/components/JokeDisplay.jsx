d0521d7656687a855838737ca2151f20a790d553
Melanie Rodriguez <melarodrigg@gmail.com>
2025-12-06 20:48:38 -0600

    Lab Completed

--- src/components/JokeDisplay.jsx
+++ src/components/JokeDisplay.jsx
@@ -4,6 +4,7 @@ const JokeDisplay = ({ joke, loading }) => {
   return (
     <div className="joke-container">
       {/* Step 2: If `loading` is true, display "Loading..." */}
+      {loading ? <p>Loading...</p> : <p>{joke}</p>}
       {/* Step 3: Otherwise, display the joke */}
     </div>
   )
